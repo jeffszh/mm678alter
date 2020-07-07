01d2e0b59c1ee6164a540cd2d64a38e611d3a2d5
Jeff <zhsu@amwares.com>
2020-07-07 15:55:29 +0800

    弄好 Apply.bat

--- Apply.bat
+++ Apply.bat
@@ -1,3 +1,5 @@
 set mmRoot=e:/mm678 merged
-echo "%mmRoot%/scripts/global"
+copy NPCFollowers.lua "%mmRoot%/Scripts/General/"
+copy NPCFollowersSkills.lua "%mmRoot%/Scripts/Global/"
+copy ExtraArtifacts.lua "%mmRoot%/Scripts/Global/"
 pause
