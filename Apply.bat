07909fe0ceb24d3b252d5017443ee44516cd4aa5
jeffszh <jeffszh@126.com>
2020-07-10 05:07:51 +0800

    修正 Apply.bat

--- Apply.bat
+++ Apply.bat
@@ -2,5 +2,5 @@ set mmRoot=e:/mm678 merged
 copy NPCFollowers.lua "%mmRoot%/Scripts/General/"
 copy NPCFollowersSkills.lua "%mmRoot%/Scripts/Global/"
 copy ExtraArtifacts.lua "%mmRoot%/Scripts/Global/"
-copy Class Skills.txt "%mmRoot%Data/Tables/"
+copy "Class Skills.txt" "%mmRoot%/Data/Tables/"
 pause
